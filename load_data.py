8b31963e647433e9cff12db8315523643e218a7c
jonnir <nir.jonathan@gmail.com>
2026-01-23 18:21:10 -0500

    fix bug when loading dataset

--- load_data.py
+++ load_data.py
@@ -14,18 +14,16 @@ def load_godwin2025(path: str = None) -> pd.DataFrame:
         pd.read_excel(path)
         .dropna(subset=["PAPER_LINK"])
         .drop_duplicates(subset=["PAPER_LINK"])
-        .assign(data_sharing_class=lambda df: _assign_data_sharing_class(df))
+        .assign(data_sharing_class=lambda df: _assign_eyemovement_sharing_class(df))
     )
     dataset = dataset[dataset["PAPER_LINK"] != "UNPUBLISHED"]
     return dataset
 
 
-def _assign_data_sharing_class(df: pd.DataFrame) -> pd.Series:
+def _assign_eyemovement_sharing_class(df: pd.DataFrame) -> pd.Series:
     sharing_class = pd.Series("NONE", index=df.index, dtype="string").rename("data_sharing_class")
     # specify data from coarse to finest to overwrite when article shares more fine-grained data
     sharing_class[df["BY_PPT"] == "YES"] = "PPT"
     sharing_class[df["BY_TRIAL"] == "YES"] = "TRIAL"
     sharing_class[df["BY_FIXATION"] == "YES"] = "FIXATION"
-    # fill "unknown" if article is marked as "actually shared"
-    sharing_class[(df["ACTUALLY_SHARED"] == "YES") & (sharing_class == "NONE")] = "UNKNOWN"
     return sharing_class
