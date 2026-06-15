c30df58964de7f6e5a7685c7b11cca2d0c9d6894
jonnir <nir.jonathan@gmail.com>
2026-06-15 18:03:05 -0400

    move metadata loading + merging with original Godwin dataset to `load_data.py`

--- load_data.py
+++ load_data.py
@@ -2,6 +2,23 @@ import os
 
 import pandas as pd
 
+from fetch_metadata import fetch_all_metadata
+
+
+def prepare_analytical_dataset(godwin_path: str, metadata_path: str) -> pd.DataFrame:
+    godwin_dataset = load_godwin2025(godwin_path)
+    metadata = _load_or_fetch_metadata(metadata_path, godwin_dataset)
+    merged = (
+        pd.concat([metadata, godwin_dataset], axis=1)
+        .assign(
+            is_sharing_data=lambda df: df["data_sharing_class"] != "NONE",
+            is_sharing_anything=lambda df: (df[[
+                "EXPERIMENT", "MATERIALS", "CODE", "CODEBOOK_GUIDE", "BY_FIXATION", "BY_TRIAL", "BY_PPT"
+            ]] == "YES").any(axis=1)
+        )
+    )
+    return merged
+
 
 def load_godwin2025(path: str = None) -> pd.DataFrame:
     if not path:
@@ -29,3 +46,15 @@ def _assign_eyemovement_sharing_class(df: pd.DataFrame) -> pd.Series:
     sharing_class[df["BY_TRIAL"] == "YES"] = "TRIAL"
     sharing_class[df["BY_FIXATION"] == "YES"] = "FIXATION"
     return sharing_class
+
+
+def _load_or_fetch_metadata(path: str, godwin_dataset: pd.DataFrame) -> pd.DataFrame:
+    try:
+        metadata = pd.read_csv(os.path.join(path), index_col=0)
+        print("Loaded OpenAlex metadata from CSV.")
+    except FileNotFoundError:
+        print("Fetching metadata from OpenAlex. This may take a few minutes...")
+        metadata = fetch_all_metadata(godwin_dataset, sleep_period=0.01, verbose=True)
+        metadata.to_csv(os.path.join(os.getcwd(), "Godwin_2025_metadata.csv"), index=True)
+    metadata["Pub2UpdateTime"] = pd.to_timedelta(metadata["Pub2UpdateTime"])    # validate casting
+    return metadata
